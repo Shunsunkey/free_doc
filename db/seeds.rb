42dbbe259614241d27b2580bf5917b6234492358
root <root@DESKTOP-537TTPD>
2024-04-25 09:56:15 +0200

    modification du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+5.times do
+    Doctor.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      specialty: Faker::Medical::Specialties.type,
+      zip_code: Faker::Address.zip_code
+    )
+  end
+
+  10.times do
+    Patient.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name
+    )
+  end
+
+doctors = Doctor.all
+patients = Patient.all
+
+20.times do
+    doctor = doctors.sample
+    patient = patients.sample
+    Appointment.create(
+      date: Faker::Time.between(from: DateTime.now, to: 1.year.from_now),
+      doctor: doctor,
+      patient: patient
+    )
+end
